a66226a44045c12bd0f044dab26e8fd77485b418
joseramonhq <joseramonhq@gmail.com>
2017-06-20 16:10:49 +0200

    Update S05Ejercicio24.java (#2)
    Otra manera de hacerlo

--- soluciones/05_Bucles/S05Ejercicio24.java
+++ soluciones/05_Bucles/S05Ejercicio24.java
@@ -8,40 +8,45 @@
  *        12321 
  *       1234321
  * 
- * @author Luis José Sánchez
+ * @author Jose Ramon Hidalgo
  */
-public class S05Ejercicio24 {
+import java.util.Scanner;
+public class Ejercicio24Pagina47 {
 
-  public static void main(String[] args) {
-    
-    System.out.println("Este programa pinta una pirámide hecha a base de números.");
-    System.out.print("Por favor, introduzca la altura de la pirámide:");
-    int alturaIntroducida = Integer.parseInt(System.console().readLine());
+	public static void main(String[] args) {
+	
+		Scanner input=new Scanner(System.in);
+		System.out.println("Dame la altura de la piramide");
+		int altura=input.nextInt();
+		
+		int a=0;
+		
+		// Este comando for cuenta las filas que se van generando hasta las introducidas 
+		for (int i=1;i<=altura;i++){
+			//Este otro for generará espacios delante de los numeros que vendrán después
+			
+			for (int k=0;k<=altura-a;k++){
+				System.out.print(" ");
+			}
+			
+			//Este for imprime los numeros hasta el numero mayor, que el el numero de fila en el que está el primer for
+			for (int v=1;v<=i;v++){
+				System.out.print(v);
+			}
+			
+			/** Este for hace que se impriman los números hasta el 1 y no repitiendo el numero máximo, que es el 
+			 * de la fila en que está el programa 
+			 */
+			
+			for (int r=i-1;r>0;r--){
+				if (r>0){
+					System.out.print(r);
+				}
+			}
+			System.out.println("");
+			a++;
+		}
 
-    int altura = 1;
-    int i = 0;
-    int espacios = alturaIntroducida - 1;
-    
-    while (altura <= alturaIntroducida) {
-      
-      // inserta espacios
-      for (i = 1; i <= espacios; i++) {
-        System.out.print(" ");
-      }
-      
-      // pinta la línea de números
-      for (i = 1; i < altura; i++) {
-        System.out.print(i);
-      }
-      
-      for (i = altura; i > 0; i--) {
-        System.out.print(i);
-      }
-      
-      System.out.println();
-      
-      altura++;
-      espacios--;
-    } // while
-  }
+	}
+
 }
